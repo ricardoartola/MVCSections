228d118eb2d7794feecc7be3b782b1621ca22734
Ricardo Artola <rartola@outlook.com>
2021-03-04 09:15:00 -0500

    Add project files.

---
+++ MVCSections/Helpers/HTMLHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSections.Helpers
+{
+    public static class HTMLHelperExtensions
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string imageUrl, string alt)
+        {
+            TagBuilder builder = new TagBuilder("img");
+            builder.MergeAttribute("src", "/images/" + imageUrl);
+            builder.MergeAttribute("alt", alt);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
---
+++ MVCSections/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSections.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
